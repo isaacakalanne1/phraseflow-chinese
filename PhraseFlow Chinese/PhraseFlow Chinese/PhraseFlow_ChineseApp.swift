5723d59d9a0082cdc9a84fe9a5b0ac85c5c8d2e1
Isaac Akalanne <isaacakalanne@gmail.com>
2024-09-07 14:01:47 +0100

    Initial project

---
+++ PhraseFlow Chinese/PhraseFlow Chinese/PhraseFlow_ChineseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhraseFlow_ChineseApp.swift
+//  PhraseFlow Chinese
+//
+//  Created by iakalann on 07/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhraseFlow_ChineseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
