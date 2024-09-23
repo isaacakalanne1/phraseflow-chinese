0136df412d686255538ab38f77e86ec393ece08b
Isaac Akalanne <isaacakalanne@gmail.com>
2024-09-23 01:04:38 +0100

    Further in progress updates

--- PhraseFlow Chinese/PhraseFlow Chinese/Extensions/String+Tone.swift
+++ PhraseFlow Chinese/PhraseFlow Chinese/Extensions/String+Tone.swift
@@ -9,28 +9,42 @@ import Foundation
 
 extension String {
     func convertToneNumberToDiacritic() -> String {
-        let tones: [Character: [Character]] = [
-            "a": ["ā", "á", "ǎ", "à"],
-            "e": ["ē", "é", "ě", "è"],
-            "i": ["ī", "í", "ǐ", "ì"],
-            "o": ["ō", "ó", "ǒ", "ò"],
-            "u": ["ū", "ú", "ǔ", "ù"],
-            "ü": ["ǖ", "ǘ", "ǚ", "ǜ"]
-        ]
-        guard let toneMarker = self.last else { return self }
-        let toneMarkerString = String(toneMarker)
-        guard let toneMarkerIndex = Int(toneMarkerString) else { return self }
-        var characterIndex = 0
-        var updatedPinyin = self
+        var allPinyin = ""
+        let words = self.components(separatedBy: " ")
+        for word in words {
+            var newPinyin = ""
+            let tones: [Character: [Character]] = [
+                "a": ["ā", "á", "ǎ", "à"],
+                "e": ["ē", "é", "ě", "è"],
+                "i": ["ī", "í", "ǐ", "ì"],
+                "o": ["ō", "ó", "ǒ", "ò"],
+                "u": ["ū", "ú", "ǔ", "ù"],
+                "ü": ["ǖ", "ǘ", "ǚ", "ǜ"]
+            ]
+            guard let toneMarker = word.last else { return self }
+            let toneMarkerString = String(toneMarker)
+            guard let toneMarkerIndex = Int(toneMarkerString) else { return self }
+            var characterIndex = 0
 
-        for character in self {
-            guard let toneList = tones[character],
-                  toneList.count > toneMarkerIndex else { continue }
-            let newTone = String(toneList[toneMarkerIndex])
-            updatedPinyin = updatedPinyin.prefix(characterIndex) + newTone + updatedPinyin.dropFirst(characterIndex)
-            break
+            for character in self {
+                guard let toneList = tones[character],
+                      toneList.count > toneMarkerIndex else {
+                    characterIndex += 1
+                    continue
+                }
+                let newTone = String(toneList[toneMarkerIndex])
+                newPinyin = word.prefix(characterIndex) + newTone + word.dropFirst(characterIndex)
+                break
+            }
+            if newPinyin.isNotEmpty {
+                if allPinyin.isEmpty {
+                    allPinyin.append(newPinyin)
+                } else {
+                    allPinyin.append(" \(newPinyin)")
+                }
+            }
         }
-        return updatedPinyin
+        return allPinyin
     }
 
 }
