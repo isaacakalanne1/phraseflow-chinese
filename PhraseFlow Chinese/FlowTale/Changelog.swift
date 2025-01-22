4dd7a49a7b0921a24a714dfadd61aaad160040d5
Isaac Akalanne <isaacakalanne@gmail.com>
2025-01-22 16:53:49 +0000

    Added Changelog

---
+++ PhraseFlow Chinese/FlowTale/Changelog.swift
@@ -0,0 +1,18 @@
+//
+//  Changelog.swift
+//  FlowTale
+//
+//  Created by iakalann on 22/01/2025.
+//
+
+import Foundation
+
+struct Changelog {
+    static var changes: [String: [String]] {
+        [
+            "1.1": [
+                "Updated tab bar to be more clear"
+            ]
+        ]
+    }
+}
