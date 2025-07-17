894a41b7dd67c9cb56e55c3aa4107961718add43
Isaac Akalanne <isaacakalanne@gmail.com>
2025-07-17 11:25:23 +0100

    Moving to packages

---
+++ PhraseFlow Chinese/APIRequest/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "APIRequest",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "APIRequest",
+            targets: ["APIRequest"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "APIRequest"),
+        .testTarget(
+            name: "APIRequestTests",
+            dependencies: ["APIRequest"]
+        ),
+    ]
+)
