6cff8891caabcc1074f72ada5f446058c43f675b
Isaac Akalanne <isaacakalanne@gmail.com>
2025-07-18 22:43:57 +0100

    Added microsoft speech framework

--- PhraseFlow Chinese/Speech/Package.swift
+++ PhraseFlow Chinese/Speech/Package.swift
@@ -17,8 +17,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "MicrosoftCognitiveServicesSpeech",
+            path: "Frameworks/MicrosoftCognitiveServicesSpeech.xcframework"
+        ),
         .target(
-            name: "Speech"
+            name: "Speech",
+            dependencies: [
+                "MicrosoftCognitiveServicesSpeech"
+            ]
         ),
         .testTarget(
             name: "SpeechTests",
