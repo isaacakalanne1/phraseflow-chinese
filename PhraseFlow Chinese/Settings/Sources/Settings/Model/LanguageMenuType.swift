ae1ba64614ac7ef0bbb01210d1e15fcdff099b3f
Isaac Akalanne <isaacakalanne@gmail.com>
2025-09-18 23:57:42 +0100

    Removed unused enum case

--- PhraseFlow Chinese/Settings/Sources/Settings/Model/LanguageMenuType.swift
+++ PhraseFlow Chinese/Settings/Sources/Settings/Model/LanguageMenuType.swift
@@ -9,12 +9,10 @@ public enum LanguageMenuType {
     case normal
     case translationSourceLanguage
     case translationTargetLanguage
-    case translationTextLanguage
     
     var shouldShowAutoDetect: Bool {
         switch self {
         case .normal,
-                .translationTextLanguage,
                 .translationTargetLanguage:
             return false
         case .translationSourceLanguage:
