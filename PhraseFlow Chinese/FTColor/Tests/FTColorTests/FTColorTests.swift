3220b7e3b976ba76ba5259c9edcc7d9c487b97d6
Isaac Akalanne <isaacakalanne@gmail.com>
2025-09-19 18:35:12 +0100

    Added FTColorTests (all passing)

--- PhraseFlow Chinese/FTColor/Tests/FTColorTests/FTColorTests.swift
+++ PhraseFlow Chinese/FTColor/Tests/FTColorTests/FTColorTests.swift
@@ -1,11 +1,62 @@
 import Testing
+import SwiftUI
 @testable import FTColor
 
 final class FTColorTests {
 
     @Test
     func accent() throws {
-        let color = FTColor.accent.color
-        #expect(color.name)
+        let color = FTColor.accent
+        let expectedName = "FTAccent"
+        #expect(color.rawValue == expectedName)
+        #expect(color.color == Color(expectedName, bundle: .module))
+    }
+
+    @Test
+    func background() throws {
+        let color = FTColor.background
+        let expectedName = "FTBackground"
+        #expect(color.rawValue == expectedName)
+        #expect(color.color == Color(expectedName, bundle: .module))
+    }
+
+    @Test
+    func error() throws {
+        let color = FTColor.error
+        let expectedName = "FTError"
+        #expect(color.rawValue == expectedName)
+        #expect(color.color == Color(expectedName, bundle: .module))
+    }
+
+    @Test
+    func highlight() throws {
+        let color = FTColor.highlight
+        let expectedName = "FTHighlight"
+        #expect(color.rawValue == expectedName)
+        #expect(color.color == Color(expectedName, bundle: .module))
+    }
+
+    @Test
+    func primary() throws {
+        let color = FTColor.primary
+        let expectedName = "FTPrimary"
+        #expect(color.rawValue == expectedName)
+        #expect(color.color == Color(expectedName, bundle: .module))
+    }
+
+    @Test
+    func secondary() throws {
+        let color = FTColor.secondary
+        let expectedName = "FTSecondary"
+        #expect(color.rawValue == expectedName)
+        #expect(color.color == Color(expectedName, bundle: .module))
+    }
+
+    @Test
+    func wordHighlight() throws {
+        let color = FTColor.wordHighlight
+        let expectedName = "FTWordHighlight"
+        #expect(color.rawValue == expectedName)
+        #expect(color.color == Color(expectedName, bundle: .module))
     }
 }
